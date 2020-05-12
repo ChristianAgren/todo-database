75bb938c31daf12e8ab64e5596535baa86e865a5
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-05-12 14:00:36 +0200

    added proxy to /session

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -7,4 +7,11 @@ module.exports = function(app) {
       changeOrigin: true,
     })
   );
+  app.use(
+    '/session',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+    })
+  );
 };
