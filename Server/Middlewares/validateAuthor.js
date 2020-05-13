2aa58d8342e5061f75eae21dbdb531e4f169b445
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-05-13 18:05:28 +0200

    created middleware for validating changes

--- Server/Middlewares/validateAuthor.js
+++ Server/Middlewares/validateAuthor.js
@@ -1,10 +1,24 @@
 
 module.exports = function (req, res, next) {
+    console.log('validating changes');
+    console.log(req.body);
+    
     if (res.assignment) {
-        if (res.assignment.parentId === req.session.id || req.session.admin) {
+        console.log('found assignment');
+        
+        if (res.assignment.parentId == req.session.id || req.session.admin) {
+            console.log('valid user');
             next()
         }
-    }   else {
+    } else if (req.body.assignmentParentId) {
+        console.log('found subtask');
+
+        if (req.body.assignmentParentId == req.session.id || req.session.admin) {
+            console.log('valid user');
+            next()
+        }
+    } else {
+        console.log('invalid user');
         res.status(401).json({ message: 'Unauthorized' })
     }
 }
