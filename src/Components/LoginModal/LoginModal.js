9ef141727ab6bb570205b6365ced50d24645ff48
Fred Malm <fredrik.malmborg@medieinstitutet.se>
2020-05-13 15:34:15 +0200

    broke out css: loginModal

--- src/Components/LoginModal/LoginModal.js
+++ src/Components/LoginModal/LoginModal.js
@@ -1,5 +1,9 @@
-// @ts-nocheck
 import React from "react";
+
+// COMPONENTS
+import UserMenu from "./UserMenu/UserMenu";
+
+// MATERIAL UI
 import {
   Button,
   FormControl,
@@ -9,87 +13,24 @@ import {
   InputAdornment,
   InputLabel,
   IconButton,
-  makeStyles,
   Modal,
   TextField,
   Typography,
 } from "@material-ui/core/";
-import { UserContext } from "../../Contexts/UserContext";
-import UserMenu from "./UserMenu/UserMenu";
+
+// ICONS
 import AccountBoxIcon from "@material-ui/icons/AccountBox";
 import AddBoxIcon from "@material-ui/icons/AddBox";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
 
-const useStyles = makeStyles((theme) => ({
-  loginContainer: {
-    "& .MuiButton-label": {
-      color: "white",
-    },
-  },
-  paper: {
-    position: "absolute",
-    top: "53%",
-    left: "50%",
-    transform: "translateX(-50%) translateY(-50%)",
+// CONTEXTS
+import { UserContext } from "../../Contexts/UserContext";
 
-    width: "40%",
-    minWidth: "20rem",
-    height: "auto",
+// STYLES
+import useStyles from "./LoginModalStyles";
 
-    backgroundColor: theme.palette.background.paper,
-    borderRadius: ".4rem",
-    borderBottom: ".4rem solid rgb(17,82,147)",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    [theme.breakpoints.down(321)]: {
-      width: "100%",
-      borderRadius: 0,
-    },
-  },
-  loginModal: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    color: "rgb(17,82,147)",
-    "& .MuiGrid-item": {
-      "& > .MuiSvgIcon-root": {
-        position: "absolute",
-        top: "-2rem",
-        left: "50%",
-        transform: "translateX(-50%)",
-        fontSize: "5rem",
-        backgroundColor: theme.palette.background.paper,
-        borderRadius: ".4rem",
-      },
-    },
-    "& .MuiTypography-h4": {
-      padding: theme.spacing(5, 0, 3, 0),
-    },
-    "& .MuiFormControl-root": {
-      margin: theme.spacing(1, 0),
-      [theme.breakpoints.down(400)]: {
-        margin: theme.spacing(0.2, 0),
-      },
-    },
-  },
-  loginBtn: {
-    margin: theme.spacing(3, 0),
-    width: "50%",
-    "& .MuiButton-outlinedPrimary": {
-      width: "100%",
-    },
-  },
-  registerContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    "& .MuiTypography-overline": {
-      fontSize: "0.84rem",
-      margin: theme.spacing(0, 1),
-    },
-  },
-}));
+// -- -- --
 
 function LoginModal(props) {
   const classes = useStyles();
