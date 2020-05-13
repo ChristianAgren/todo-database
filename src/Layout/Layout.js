f6a3675248e1a48a1e49e18a9f785209573eeaef
Fred Malm <fredrik.malmborg@medieinstitutet.se>
2020-05-13 15:35:44 +0200

    broke out css: layout

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,38 +1,35 @@
-// @ts-nocheck
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Topbar from '../Components/TopBar/Topbar';
-import UserManager from "../Components/UserManager/UserManager"
-import Main from '../Components/Main/Main'
-import { UserContext } from '../Contexts/UserContext';
+import React from "react";
 
-const useStyles = makeStyles((theme) => ({
-    mainContainer: {
-        minHeight: "100vh",
-        height: "100%",
-        backgroundColor: "#F5F5F5",
-    },
-}));
+// COMPONENTS
+import Topbar from "../Components/TopBar/Topbar";
+import UserManager from "../Components/UserManager/UserManager";
+import Main from "../Components/Main/Main";
+
+// CONTEXTS
+import { UserContext } from "../Contexts/UserContext";
+
+// STYLES
+import useStyles from "./LayoutStyles";
+
+// -- -- --
 
 function Layout() {
-    const classes = useStyles()
-    const [view, setView] = React.useState('main')
-    const handleChangeView = (view) => {
-        setView(view)
-    }
+  const classes = useStyles();
+  const [view, setView] = React.useState("main");
+  const handleChangeView = (view) => {
+    setView(view);
+  };
 
-    return (
-        <UserContext.Consumer>
-            {user => (
-                <div className={classes.mainContainer}>
-                    <Topbar changeView={handleChangeView} />
-                    {view === 'main' ?
-                        <Main /> : <UserManager user={user} />
-                    }
-                </div>
-            )}
-        </UserContext.Consumer>
-    )
+  return (
+    <UserContext.Consumer>
+      {(user) => (
+        <div className={classes.mainContainer}>
+          <Topbar changeView={handleChangeView} />
+          {view === "main" ? <Main /> : <UserManager user={user} />}
+        </div>
+      )}
+    </UserContext.Consumer>
+  );
 }
 
-export default Layout
+export default Layout;
---
+++ src/Layout/LayoutStyles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    minHeight: "100vh",
+    height: "100%",
+    backgroundColor: "#F5F5F5",
+  },
+}));
+
+export default useStyles;
